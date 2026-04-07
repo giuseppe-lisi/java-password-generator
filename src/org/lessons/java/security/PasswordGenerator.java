72d8a67ec9405d2bbdf35e041ccbe686ce205d49
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-04-08 00:07:33 +0200

    creato programma per generare la password basata sull'input dell'utente

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,7 +1,29 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
     public static void main(String[] args) {
-        System.out.println("programma generatore di password");
+        String password;
+
+        // creates new instance of Scanner class
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Type your name: ");
+        String name = in.nextLine();
+        System.out.print("Type your surname: ");
+        String surname = in.nextLine();
+        System.out.print("Type your favorite color: ");
+        String favColor = in.nextLine();
+        System.out.print("Type your birth year: ");
+        int birthYear = in.nextInt();
+        System.out.print("Type your birth month: ");
+        int birthMonth = in.nextInt();
+        System.out.print("Type your birth day: ");
+        int birthDay = in.nextInt();
+
+        password = name + "-" + surname + "-" + favColor + "-" + birthYear + birthMonth + birthDay;
+        System.out.println(password);
+
     }
 }
